54075f6898f1a9270b13e814d36036f1cb08a540
Irene López <irene.lopezs@protonmail.com>
2022-05-03 15:48:34 +0100

    iter 2

--- text_similarity/demo.py
+++ text_similarity/demo.py
@@ -6,17 +6,30 @@ from src.search import load_embeddings
 def search(query):
 
     # Search the embeddings
-    results = embeddings.search(query, 1)
-    best_match = results[0]
+    best_match_label = embeddings_label.search(query, 1)[0]
+    best_match_synonym = embeddings_synonym.search(query, 1)[0]
 
     # Return the results
-    print(f"Best match for {query} is {best_match['text']} ({best_match['id']}) with score {best_match['score']}")
-    return best_match
+    return best_match_label, best_match_synonym
 
 if __name__ == "__main__":
 
-    embeddings_path = 'embeddings/embeddings_synonyms.tar.gz'
-    embeddings = load_embeddings(embeddings_path)
-    demo = gr.Interface(fn=search, inputs='text', outputs='text')
+    # Load both models
+    embeddings_label_path = 'embeddings/embeddings_labels.tar.gz'
+    embeddings_synonym_path = 'embeddings/embeddings_synonyms.tar.gz'
+    embeddings_label = load_embeddings(embeddings_label_path)
+    embeddings_synonym = load_embeddings(embeddings_synonym_path)
+
+    demo = gr.Interface(
+        fn=search,
+        inputs='text',
+        outputs=['text', 'text'],
+        title='Embedding based disease mapping',
+        description="""
+        Get the most similar disease in EFO by looking at the vector similarity. Proof of concept.
+        Output 1: model only based on EFO ID/label pairs.
+        Output 2: model based on EFO ID/labels and exact synonyms pairs.""",
+        examples=['breast carcinoma', 'high cholesterol', 'ochoa syndrome', 'C-C motif chemokine measurement']
+    )
 
     demo.launch(share=True)
